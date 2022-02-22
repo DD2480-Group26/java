48ab74aadfe700db109beb2f52c50cbff3c678a4
massyv <victor.massy@grenoble-inp.org>
2022-02-22 22:04:26 +0100

    Solved issue #2, tests and comparison of the outputs

--- src/test/java/com/jsoniter/TestIterImpl.java
+++ src/test/java/com/jsoniter/TestIterImpl.java
@@ -6,45 +6,49 @@ public class TestIterImpl extends TestCase{
 
 	public void testReadStringSlowPath4() throws Exception{
 		JsonIterator iter = new JsonIterator();
-		int j = 0;
+		int j = 1;
 		iter.tail = 1;
 		iter.tail = 10;
-		iter.buf = new byte[2];
+		iter.buf = new byte[3];
 		iter.buf[0]='\\';
 		iter.buf[1]='b';
+		iter.buf[2] = '"';
 		IterImpl.readStringSlowPath(iter, j);		
 	}
 	
 	public void testReadStringSlowPath5() throws Exception{
 		JsonIterator iter = new JsonIterator();
-		int j = 0;
+		int j = 1;
 		iter.tail = 1;
 		iter.tail = 10;
-		iter.buf = new byte[2];
+		iter.buf = new byte[3];
 		iter.buf[0]='\\';
 		iter.buf[1]='n';
+		iter.buf[2] = '"';
 		IterImpl.readStringSlowPath(iter, j);		
 	}
 	
 	public void testReadStringSlowPath6() throws Exception{
 		JsonIterator iter = new JsonIterator();
-		int j = 0;
+		int j = 1;
 		iter.tail = 1;
 		iter.tail = 10;
-		iter.buf = new byte[2];
+		iter.buf = new byte[3];
 		iter.buf[0]='\\';
 		iter.buf[1]='t';
-		IterImpl.readStringSlowPath(iter, j);		
+		iter.buf[2] = '"';
+		assertEquals(IterImpl.readStringSlowPath(iter, j),++j);			
 	}
 	
 	public void testReadStringSlowPath7() throws Exception{
 		JsonIterator iter = new JsonIterator();
-		int j = 0;
+		int j = 1;
 		iter.tail = 1;
 		iter.tail = 10;
-		iter.buf = new byte[2];
+		iter.buf = new byte[3];
 		iter.buf[0]='\\';
 		iter.buf[1]='f';
-		IterImpl.readStringSlowPath(iter, j);		
+		iter.buf[2] = '"';
+		assertEquals(IterImpl.readStringSlowPath(iter, j),++j);		
 	}
 }
