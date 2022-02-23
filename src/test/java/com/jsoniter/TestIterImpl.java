36456fc9f921fe2965ab4e0099d5a6a479cce400
massyv <victor.massy@grenoble-inp.org>
2022-02-23 11:37:54 +0100

    Solved issue #8, add contracts for new tests

--- src/test/java/com/jsoniter/TestIterImpl.java
+++ src/test/java/com/jsoniter/TestIterImpl.java
@@ -4,6 +4,12 @@ import junit.framework.TestCase;
 
 public class TestIterImpl extends TestCase{
 
+	/*Contract:
+	 * Here we are testing the buffer "\\b""
+	 * This buffer should go through the loop once and increase 
+	 * j by one
+	 * Expected output: j++
+	 */
 	public void testReadStringSlowPath4() throws Exception{
 		JsonIterator iter = new JsonIterator();
 		int j = 1;
@@ -13,9 +19,15 @@ public class TestIterImpl extends TestCase{
 		iter.buf[0]='\\';
 		iter.buf[1]='b';
 		iter.buf[2] = '"';
-		IterImpl.readStringSlowPath(iter, j);		
+		assertEquals(IterImpl.readStringSlowPath(iter, j),++j);	
 	}
 	
+	/*Contract:
+	 * Here we are testing the buffer "\\n""
+	 * This buffer should go through the loop once and increase 
+	 * j by one
+	 * Expected output: j++
+	 */
 	public void testReadStringSlowPath5() throws Exception{
 		JsonIterator iter = new JsonIterator();
 		int j = 1;
@@ -24,10 +36,16 @@ public class TestIterImpl extends TestCase{
 		iter.buf = new byte[3];
 		iter.buf[0]='\\';
 		iter.buf[1]='n';
-		iter.buf[2] = '"';
-		IterImpl.readStringSlowPath(iter, j);		
+		iter.buf[2] = '"';	
+		assertEquals(IterImpl.readStringSlowPath(iter, j),++j);	
 	}
 	
+	/*Contract:
+	 * Here we are testing the buffer "\\t""
+	 * This buffer should go through the loop once and increase 
+	 * j by one
+	 * Expected output: j++
+	 */
 	public void testReadStringSlowPath6() throws Exception{
 		JsonIterator iter = new JsonIterator();
 		int j = 1;
@@ -40,6 +58,13 @@ public class TestIterImpl extends TestCase{
 		assertEquals(IterImpl.readStringSlowPath(iter, j),++j);			
 	}
 	
+	
+	/*Contract:
+	 * Here we are testing the buffer "\\f""
+	 * This buffer should go through the loop once and increase 
+	 * j by one
+	 * Expected output: j++
+	 */
 	public void testReadStringSlowPath7() throws Exception{
 		JsonIterator iter = new JsonIterator();
 		int j = 1;
