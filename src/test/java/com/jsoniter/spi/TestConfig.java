a49c7854fb463a7ce4ed27b066cc47b3d58a32d1
ElisabethC <elisabeth.chen010@gmail.com>
2022-02-23 15:13:51 +0100

    feat #3, created branch test cases for branch21 and 22

--- src/test/java/com/jsoniter/spi/TestConfig.java
+++ src/test/java/com/jsoniter/spi/TestConfig.java
@@ -1,15 +1,15 @@
 package com.jsoniter.spi;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.jsoniter.TestAnnotationJsonCreator.TestObject;
-import com.jsoniter.TestGenerics.Class3;
 import com.jsoniter.output.EncodingMode;
+import com.jsoniter.output.TestAnnotationJsonIgnore.TestPrivateVariables;
 
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Test missing branches for updateBindings in Config
+ */
 public class TestConfig {
     Config cfg;
 
@@ -19,17 +19,12 @@ public class TestConfig {
                 .encodingMode(EncodingMode.REFLECTION_MODE)
                 .indentionStep(2)
                 .build();
-
     }
-
+    
     @Test
-    public void TestUpdateBindingsBranch1() {
-        ClassDescriptor desc = ClassDescriptor.getDecodingClassDescriptor(new ClassInfo(Class3.class), true);
-        desc.fields = new ArrayList<Binding>();
-        desc.setters = null;
-        desc.getters = null;
-        desc.ctor = null;
-        desc.bindingTypeWrappers = null;
+    public void TestUpdateBindingsBranch21() {
+        ClassDescriptor desc = ClassDescriptor.getEncodingClassDescriptor(new ClassInfo(TestPrivateVariables.class), true);
+        desc.getters.get(0).name = "test";
         cfg.updateClassDescriptor(desc);
     }
 
@@ -38,11 +33,5 @@ public class TestConfig {
         ClassDescriptor desc = ClassDescriptor.getDecodingClassDescriptor(new ClassInfo(TestObject.class), true);
         desc.getters = null;
         cfg.updateClassDescriptor(desc);
-    }
-
-
-    public static void main(String[] args) {
-        TestConfig tc = new TestConfig();
-        tc.TestUpdateBindingsBranch22();
     }
 }
